1b06049b11484cad42d9c7904a294b313be95c21
athiyyahalvita <101868591+athiyyahalvita@users.noreply.github.com>
2022-12-17 22:38:14 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,39 +2,45 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class  Main {
 
     public static void main(String[] args) {
         int choise;
         double berat;
         String nama;
 
-        PaketKilat klt=new PaketKilat();
-        PaketReguler rglr=new PaketReguler();
+
+        PaketKilat klt= new PaketKilat();
+        PaketReguler rglr= new PaketReguler();
         Scanner in = new Scanner(System.in);
 
         while (true) {
-            System.out.println("====== Selamat datang ======");
-            System.out.print("Nama : ");
-            nama = in.nextLine();
-            System.out.print("Berat Laundryan : ");
-            berat = in.nextDouble();
+            System.out.println("=======================================");
+            System.out.println("====== Selamat datang di laundry ======");
+            System.out.println("=======================================");
 
+            System.out.println("---------------------------------------");
             System.out.println("==== daftar paket ====");
-            System.out.println("[1] reguler ");
-            System.out.println("[2] kilat ");
+            System.out.println("[1] kilat ");
+            System.out.println("[2] reguler ");
             System.out.print("pilih paket diatas = ");
             choise=in.nextInt();
 
-
-
             switch (choise){
                 case 1:
+                    System.out.println("---------------------------------------");
                     System.out.println("anda memilih paket kilat");
+                    klt.inputNota();
                     klt.nota();
+
+                    break;
+
                 case 2:
+                    System.out.println("---------------------------------------");
                     System.out.println("anda memilih paket reguler");
+                    rglr.inputNota();
                     rglr.nota();
+                    break;
                 default:
                     System.out.println("paket tidak ada");
             }
