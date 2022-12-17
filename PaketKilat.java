9d8ad6116025bfcff55a42e6280bf82eceab74d2
athiyyahalvita <101868591+athiyyahalvita@users.noreply.github.com>
2022-12-17 22:43:27 +0700

    Update PaketKilat.java

--- PaketKilat.java
+++ PaketKilat.java
@@ -3,16 +3,52 @@ package com.company;
 import java.util.Scanner;
 
 public class PaketKilat extends Laundry {
-//	private double bera= ;
-//	Scanner in = new Scanner(System.in);
+
+	private String nama;
+	private double berat;
+	private boolean paketKlt;
+	private double total;
+	private double totalHarga;
+
+	Scanner in = new Scanner(System.in);
+
+	public PaketKilat() {
+		this.paketKlt = false;
+	}
+
+	@Override
+	public void inputNota() {
+		paketKlt =true;
+		System.out.print("Nama : ");
+		nama = in.nextLine();
+		setNama(nama);
+		System.out.print("Berat Laundryan : ");
+		berat = in.nextDouble();
+		setBerat(berat);
+
+
+	}
 
 	@Override
 	public void nota() {
 		System.out.println(" = = =  NOTA LAUNDRY = = = ");
 		System.out.println("atas nama : "+getNama());
 		System.out.println("berat : "+getBerat());
-		System.out.print("paket yang dipilih : "+getPilihan());
-		System.out.println("Paket kilat (10000/kg)");
+//		System.out.println("Paket kilat (15.000/kg)");
+		System.out.println("= = = = = = =  = = = = = = = = = =");
+		System.out.println("=== nota jangan sampai hilang ===");
+		System.out.println("= = = = = = = = = = = = = = = = = =");
+		totalHarga();
 	}
 
+	@Override
+	public void totalHarga() {
+		if(paketKlt ==true)
+		{
+			System.out.println("paket kilat = 15000/kg");
+			totalHarga=PAKET_KILAT*getBerat();
+			System.out.println("total harga yang harus dibayar : "+totalHarga);
+
+		}
+	}
 }
